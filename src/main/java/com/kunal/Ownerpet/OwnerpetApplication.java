1a5a06b464a5f3673c59b03b3a785a03c94a2aff
Kunal Raj S <kun59211@gmail.com>
2025-08-13 23:57:01 +0530

    started with a new project

---
+++ src/main/java/com/kunal/Ownerpet/OwnerpetApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.Ownerpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/Ownerpet/OwnerpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.Ownerpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
